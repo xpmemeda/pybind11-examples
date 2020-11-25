8cc6a6e09b257d533ea986f1871eaf0a24955519
Peng Xiong <xiongpeng@megvii.com>
2020-11-25 22:50:09 +0800

    feat(src/main): element add in list

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ a = cmake_example.add(1, 2)
 b = cmake_example.sub(1, 3)
 
 print("add: %d\nsub: %d" %(a, b))
+
+a = [1, 2, 3]
+
+b = [4, 5, 6]
+
+print(a + b)
+
+print(type(a + b))
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <pybind11/pybind11.h>
+#include <vector>
+#include <assert.h>
 
 int add(int i, int j) {
     return i + j;
 }
+
+
+std::vector<int> add_vector(const std::vector<int>& x, const std::vector<int>& y) {
+    assert(x.size() == y.size());
+    std::vector<int> z(x.size());
+    for (int i = 0; i < x.size(); ++i) {
+        z[i] = x[i] + y[i];
+    }
+    return z;
+}
+
 
 namespace py=pybind11;
 
@@ -10,4 +23,5 @@ PYBIND11_MODULE(cmake_example, m) {
     m.doc() = "pybind11 cmake example";
     m.def("add", &add, "A function which adds two numbers");
     m.def("sub", [](int i, int j) { return i - j; }, "A function which subtract two numbers");
+    m.def("add_vector", &add_vector);
 }
